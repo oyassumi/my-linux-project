2598d8ae29326b3e3b72f4d9511c815fc21b252d
oyassumi <galeri45@yandex.ru>
2025-08-23 10:20:29 +0300

    Добавлены script3.sh и script4.sh

---
+++ script3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dir_path=~/my_first_steps_in_Linux/
+file_path=${dir_path}test_file.txt
+
+echo "Creating file $file_path"
+touch $file_path
+echo "Checking content of dir $dir_path"
+ls $dir_path
